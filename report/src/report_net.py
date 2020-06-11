7e0f913b89095be4a198da8c72a842b70ef21adc
鬼澤英里 <kizawaeri@g.ecc.u-tokyo.ac.jp>
2020-06-11 15:23:31 +0900

    finding2vec要修正

--- report/src/report_net.py
+++ report/src/report_net.py
@@ -8,8 +8,8 @@ class Report2vec:
     def __init__(self,name):
 
         self.name = name
-        self.file_path = 'data/med_parsed.txt'
-        self.type_path = 'data/dependency_type.txt'
+        self.file_path = '../data/med_parsed.txt'
+        self.type_path = '../data/dependency_type.txt'
         self.w2i = {}
         self.i2w = {}
         self.vocabulary_size = None
@@ -31,6 +31,10 @@ class Report2vec:
                 if cnt > 1:
                     self.findings[cnt-1] = finding
                     finding = []
+                #########################
+                if cnt == 3:
+                    break
+                #########################
             else:
                 finding.append(list(l for l in line.split()))
 
@@ -38,16 +42,17 @@ class Report2vec:
 
         ID = 1
 
-        for key in self.finding:
+        for key in self.findings:
             finding = self.findings[key]
             for j in range(len(finding)):
-                if 'Token' in finding[j]:
-                    word = finding[j][1][6:-1]
+                if 'Token' in finding[j][0]:
+                    word = finding[j][1][6:-2]
                     if self.w2i.get(word) == None:
                         self.w2i[word] = ID
                         ID += 1
         
         self.w2i['unknown_word'] = ID
+        self.w2i['root_id'] = ID + 1
         self.i2w = {i: w for w, i in self.w2i.items()}
 
         self.vocabulary_size = len(self.w2i)
@@ -62,47 +67,102 @@ class Report2vec:
     
     def finding2vec(self):
 
+        vec = []
+        f_vec = []
+        sent = []
+
         for key in self.findings:
+            print(key)
+
             finding = self.findings[key]
+            stored = 0
+
             for i in range(len(finding)):
-                cnt = 0
-                NN_with = {}
-                with_NN = {}
-                if 'sentence' in finding[i]:
-                    cnt += 1
-                    if cnt > 1:
-                        NN_with = {}
-                        with_NN = {}
-                elif 'Token' in finding[i]:
-                    index = finding[i][0][-1]
-                    pos = finding[i][3][5:-1]
-                    word = finding[i][1][6:-1]
-                    head = finding[i][4][-1]
+
+                print('finding[i]:',finding[i])
+
+                if 'sentence' in finding[i][0]:
+
+                    if sent != []:
+                        stored = i
+                        f_vec.append(sent)
+                        sent = []
+
+                elif 'Token' in finding[i][0]:
+                    index = int(finding[i][0][-2])
+                    #print('index:',index)
+                    pos = finding[i][3][5:-2]
+                    #print('pos:',pos)
+                    word = finding[i][1][6:-2]
+                    #print('word:',word)
+                    head = int(finding[i][4][5:-1])+stored
+                    print('head:',head)
                     R = finding[i][5][8:-2]
+                    #print('R:',R)
+
+                    '''
+                    if head != 0:
+                        print('head:',finding[head])
+                        print('head pos:',finding[head][3])
+                        print('head pos head:',finding[head][3][5])
+                    '''
+
                     if pos == 'NN':
-                        NN_with[word] = [finding[head][1][6:-1],R]
-                    elif finding[head][3][5:-1] == 'NN':
-                        with_NN[word] = [finding[head][1][6:-1],R]
+                        ID = self.w2i[word]
+                        if 'sentence' in finding[head][0]:
+                            with_id = self.w2i['root_id']
+                        else:                           
+                            with_id = self.w2i[finding[head][1][6:-2]]
+                        sent.append([ID,with_id,R]) 
                     else:
-                        pass
+                        head = finding[head]
+                        if head[0] != 'sentence':
+                            if head[3][5:-2] == 'NN':
+                                ID = self.w2i[word]
+                                with_id = self.w2i[head[1][6:-2]]
+                                sent.append([ID,with_id,R])
+                            else:
+                                pass
+                        else:
+                            pass
+
+                elif i == len(finding) - 1:
+                    f_vec.append(sent)
+                    vec.append(f_vec)
+                    f_vec = []
+
                 else:
                     pass
+        
+        if f_vec != []:
+            vec.append(f_vec)
 
         return vec
 
 class MyDataset(torch.utils.data.Dataset):
 
     def __init__(self,data):
+        self.data = data
         pass
     
     def __len__(self):
-        pass
+        return len(self.data)
     
     def __getitem__(self,idx):
         pass
 
 if __name__ == '__main__':
 
-    word_dct = load_obj('data/dct.pickle')
-    findings = separate_each_finding('data/med_parsed.txt')
-    save_obj(findings,'data/finding.pickle')
+    v = Report2vec("IU_Xray")
+
+    v.separate_each_finding()
+    print(v.findings)
+    v.word2id()
+    cnt = 0
+    for k in v.w2i:
+        if cnt > 10:
+            break
+        print(v.w2i[k])
+        cnt += 1
+    vec = v.finding2vec()
+    print(vec)
